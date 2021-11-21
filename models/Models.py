c258fb00e30b357ff6c8c568504d9540df7c0564
Madhu-balaji-01 <madhub2303@gmail.com>
2021-11-21 05:01:10 +0000

    Updated DeepSpeech model

--- models/Models.py
+++ models/Models.py
@@ -241,23 +241,22 @@ class ResidualCNN(nn.Module):
     """Residual CNN inspired by https://arxiv.org/pdf/1603.05027.pdf
         except with layer norm instead of batch norm
     """
-    def __init__(self, in_channels, out_channels, kernel, stride, dropout, n_feats):
+    def __init__(self, norm_mode, in_channels, out_channels, kernel, stride, dropout, n_feats):
         super(ResidualCNN, self).__init__()
 
         self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel//2)
         self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel//2)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
-        self.layer_norm1 = Normalization(mode=norm_mode)
-        self.layer_norm2 = Normalization(mode=norm_mode)
+        self.norm_layer = Normalization(mode=norm_mode)
 
     def forward(self, x):
         residual = x  # (batch, channel, feature, time)
-        x = self.layer_norm1(x)
+        x = self.norm_layer(x)
         x = F.gelu(x)
         x = self.dropout1(x)
         x = self.cnn1(x)
-        x = self.layer_norm2(x)
+        x = self.norm_layer(x)
         x = F.gelu(x)
         x = self.dropout2(x)
         x = self.cnn2(x)
@@ -265,17 +264,17 @@ class ResidualCNN(nn.Module):
         return x # (batch, channel, feature, time)
 
 class BidirectionalGRU(nn.Module):
-    def __init__(self, rnn_dim, hidden_size, dropout, batch_first):
+    def __init__(self, norm_mode,rnn_dim, hidden_size, dropout, batch_first):
         super(BidirectionalGRU, self).__init__()
 
         self.BiGRU = nn.GRU(
             input_size=rnn_dim, hidden_size=hidden_size,
             num_layers=1, batch_first=batch_first, bidirectional=True)
-        self.layer_norm = nn.LayerNorm(rnn_dim)
+        self.norm_layer = Normalization(mode=norm_mode)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        x = self.layer_norm(x)
+        x = self.norm_layer(x)
         x = F.gelu(x)
         x, _ = self.BiGRU(x)
         x = self.dropout(x)
@@ -283,29 +282,31 @@ class BidirectionalGRU(nn.Module):
 
 
 class DeepSpeechModel(nn.Module):
-    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
+    def __init__(self, spec_layer, norm_mode, n_cnn_layers, n_rnn_layers, 
+                    rnn_dim, n_feats, output_dim=88,stride=2, dropout=0.1):
         super(DeepSpeechModel, self).__init__()
         n_feats = n_feats//2
+        self.spec_layer = spec_layer
+        self.norm_layer = Normalization(mode=norm_mode)
         self.cnn = nn.Conv2d(1, 32, 3, stride=stride, padding=3//2)  # cnn for extracting heirachal features
 
         # n residual cnn layers with filter size of 32
         self.rescnn_layers = nn.Sequential(*[
-            ResidualCNN(32, 32, kernel=3, stride=1, dropout=dropout, n_feats=n_feats) 
+            ResidualCNN(norm_mode=norm_mode, 32, 32, kernel=3, stride=1, dropout=dropout, n_feats=n_feats) 
             for _ in range(n_cnn_layers)
         ])
         self.fully_connected = nn.Linear(n_feats*32, rnn_dim)
         self.birnn_layers = nn.Sequential(*[
-            BidirectionalGRU(rnn_dim=rnn_dim if i==0 else rnn_dim*2,
-                              hidden_size=rnn_dim, dropout=dropout, batch_first=i==0)
+            BidirectionalGRU(norm_mode = norm_mode, rnn_dim=rnn_dim if i==0 else rnn_dim*2,
+                             hidden_size=rnn_dim, dropout=dropout, batch_first=i==0)
             for i in range(n_rnn_layers)
         ])
         self.classifier = nn.Sequential(
             nn.Linear(rnn_dim*2, rnn_dim),  # birnn returns rnn_dim*2
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(rnn_dim, n_class)
+            nn.Linear(rnn_dim, output_dim)
         )
-
     def forward(self, inp):
         spec = self.spec_layer(inp) # (B, F, T)
         spec = torch.log(spec+1e-8)
